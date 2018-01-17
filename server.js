a8c099de89130df06ee52f50ff45066da9049ef6
Haple <aleph_santos_@hotmail.com>
2018-01-17 14:30:18 -0200

    update server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var products = [];
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3001));
 
 app.listen(app.get('port'), () => {
   console.log('listening on ' + app.get('port'));
@@ -18,31 +18,32 @@ app.use(bodyParser.json());
 app.get('/get/product/:product', (req, res) => {
 
   request(`https://www.americanas.com.br/busca?conteudo=${req.params.product}/`, function (error, response, body) {
-  if (error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
+    if (error) {
+      console.log("Error: " + error);
+    }
+    console.log("Status code: " + response.statusCode);
 
-  var $ = cheerio.load(body);
+    var $ = cheerio.load(body);
 
-  $('.card-product').each(function (index) {
-    var title = $(this).find('.card-product-name').text().trim();
-    var price = $(this).find('.value').text().trim();
-    var productLink = $(this).find('.card-product-url').attr('href');
+    $('.card-product').each(function (index) {
+      var title = $(this).find('.card-product-name').text().trim();
+      var price = $(this).find('.value').text().trim();
+      var productLink = $(this).find('.card-product-url').attr('href');
 
-    products.push({
-      "title": title,
-      "price": price,
-      "link": "https://www.americanas.com.br"+productLink
+      products.push({
+        "title": title,
+        "price": price,
+        "link": "https://www.americanas.com.br" + productLink
+      });
+
+
+
     });
-    
+
+    return res.status(200).send(products);
+
   });
-  return res.send(products);
-  //JSON.stringify(products)
-  console.log(products);
 
 });
-
-})
 
 
