4bb08fddaaf9c2613e80bc60eed5e15699e3c067
Haple <aleph_santos_@hotmail.com>
2018-01-17 14:50:44 -0200

    update readme.md

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var products = [];
 
-app.set('port', (process.env.PORT || 3001));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), () => {
   console.log('listening on ' + app.get('port'));
